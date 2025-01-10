ed55bb7bcd4d122fbddca99c09d64a476f88d730
ad1tyarana <aditya9260rana@gmail.com>
2025-01-10 15:33:05 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Welcome to oops lab");
+        System.out.print(" 2");
+    }
+}
